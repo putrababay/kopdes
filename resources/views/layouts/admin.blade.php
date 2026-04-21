933a34c3c54a588e94b2337dfa431e8eb2f324a4
putrababay <bayuanggara.bp@gmail.com>
2026-04-21 20:01:13 +0700

    update highchart

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -74,7 +74,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
     <style>
         /* Mengatur agar Select2 terlihat serasi dengan form-control */
@@ -91,7 +92,7 @@
     </style>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/11.4.1/highcharts.js"></script>
 
 
 
@@ -113,7 +114,8 @@
                     <li>
                         <hr class="dropdown-divider">
                     </li>
-                    <li><a class="dropdown-item text-danger" href="{{ route('logout') }}"><i class="fas fa-sign-out-alt me-2"></i>Keluar</a></li>
+                    <li><a class="dropdown-item text-danger" href="{{ route('logout') }}"><i
+                                class="fas fa-sign-out-alt me-2"></i>Keluar</a></li>
                 </ul>
             </div>
         </div>
